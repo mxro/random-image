bff966ebdf75d5423c80c065a29f0f8419219caf
Max Rohde <maxrohde.public@gmail.com>
2022-08-14 10:01:34 +1000

    Cycle between fixed list of images

---
+++ packages/server-side-rendering/src/lib/data.ts
@@ -0,0 +1,14 @@
+export const images = [
+  {
+    name: 'pexels-gelgas-airlangga-401213.jpg',
+    source: 'https://www.pexels.com/photo/shallow-focus-of-sprout-401213/',
+  },
+  {
+    name: 'pexels-pixabay-371589.jpg',
+    source: 'https://www.pexels.com/photo/trees-near-body-of-water-371589/',
+  },
+  {
+    name: 'pexels-veeterzy-38136.jpg',
+    source: 'https://www.pexels.com/photo/green-leafed-tree-38136/',
+  },
+];
--- packages/server-side-rendering/src/routes/$index.tsx
+++ packages/server-side-rendering/src/routes/$index.tsx
@@ -11,12 +11,25 @@ import {
 import { renderDocument } from './../_document';
 import { renderPage, hydrate } from '@goldstack/template-ssr';
 
+import { images } from './../lib/data';
+
 type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>;
+
+function rand(items) {
+  return items[(items.length * Math.random()) | 0];
+}
 
 const Index = (props: { message: string }): JSX.Element => {
   return (
     <>
-      <div className={styles.image}></div>
+      <div
+        className={styles.image}
+        style={{
+          background: `url(_goldstack/static/${
+            rand(images).name
+          }) no-repeat center center`,
+        }}
+      ></div>
     </>
   );
 };
